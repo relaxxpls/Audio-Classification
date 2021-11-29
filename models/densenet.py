0cfd61c4134d998630fc93bc7f6987916c16d1a3
Laxman Desai <lolopaw24@gmail.com>
2021-11-29 19:27:32 +0530

    👌 IMPROVE: Add densenet model and testing code

---
+++ models/densenet.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+import torchvision.models as models
+
+
+class DenseNet(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        self.model = models.densenet201(pretrained=True)
+        conv0 = self.model.features.conv0
+        self.model.features.conv0 = nn.Conv2d(
+            1,
+            conv0.out_channels,
+            kernel_size=conv0.kernel_size,
+            stride=conv0.stride,
+            padding=conv0.padding,
+        )
+        self.model.classifier = nn.Linear(1920, num_classes)
+
+    def forward(self, x):
+        output = self.model(x)
+        return output
